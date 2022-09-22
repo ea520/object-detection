c8eef1702cd475b55b45d2e37c17a1dcc425cc17
ea520 <57594552+ea520@users.noreply.github.com>
2022-09-22 12:21:37 +0100

    Update with-realsense.sh

--- src/robocup-object-detection/object_detection/src/object_detection/with-realsense.sh
+++ src/robocup-object-detection/object_detection/src/object_detection/with-realsense.sh
@@ -3,5 +3,5 @@ args=$@
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 source $SCRIPT_DIR/../../../../../openvino_2022/setupvars.sh
 sleep 5
-roslaunch computer_vision detect_objects.launch $args
+roslaunch object_detection detect_objects.launch $args
 pkill -P $$
